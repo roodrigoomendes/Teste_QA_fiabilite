ba4860c52ebd5974dc673391bc41010b516e3882
roodrigoomendes <roodrigoomendessilva@gmail.com>
2022-07-17 20:27:38 -0300

    Incluindo MochaWesome

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,19 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+
   e2e: {
+    "reporter": "mochawesome",
+    "reporterOptions":{
+      "reportDir":"Cypress/reporte/mochawesome-report",
+      "overwrite": true,
+      "html": true,
+      "json": false,
+      "timestamp":"mmddyyyy_HHMMss"
+    },
     setupNodeEvents(on, config) {
+
+
       // implement node event listeners here
     },
   },
